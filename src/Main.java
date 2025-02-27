93da4c77d22efb0c4d9fbca4b4464d129e89dada
Weicheng Wang <troy54885@gmail.com>
2025-02-26 19:09:32 -0800

    Implemented FakeExchange for unit testing in Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,23 @@ public class Main {
         // Unit Test for Cash class
 
         // Test setup
-        Exchange exchange = new NYSE("someAccessKey");
-        Cash dollar = new Cash(exchange, 100);
+        //Exchange exchange = new NYSE("someAccessKey");
+        Exchange fakeExchange = new FakeExchange();
+        Cash dollar = new Cash(fakeExchange, 100);
         System.out.println("Dollar: " + dollar.toString());
 
         // Test 'in' method of Cash
         Cash euro = dollar.in("Euro");
         System.out.println("Dollar to Euro: " + euro.toString());
+    }
+}
+
+class FakeExchange implements Exchange {
+    @Override
+    public float rate(String origin, String target) {
+        if (origin.equals("USD") && target.equals("Euro")) {
+            return 0.85f; 
+        }
+        return 1.0f; 
     }
 }
